3617deff34f931ca59950ef3f1e8297e8538ad4b
l1nux-th1ngz <137152321+l1nux-th1ngz@users.noreply.github.com>
2024-08-12 04:59:49 -0600

    Update 4.sh

--- 4.sh
+++ 4.sh
@@ -4,3 +4,5 @@ git clone https://github.com/linuxdabbler/personal-dot-files.git
 git clone https://github.com/linuxdabbler/debian-install-scripts.git
 git clone https://github.com/linuxdabbler/debinfo.git
 git clone https://github.com/linuxdabbler/syscheck.git
+git clone https://github.com/VaughnValle/elektropunk.git
+git clone https://github.com/VaughnValle/kofuku.git
