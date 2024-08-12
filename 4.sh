7374aacb75aa2f911e7cb781587e1dcb4214fa45
l1nux-th1ngz <137152321+l1nux-th1ngz@users.noreply.github.com>
2024-08-12 04:53:36 -0600

    Create 4.sh

---
+++ 4.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+git clone https://github.com/linuxdabbler/personal-dot-files.git
+git clone https://github.com/linuxdabbler/debian-install-scripts.git
+git clone https://github.com/linuxdabbler/debinfo.git
+git clone https://github.com/linuxdabbler/syscheck.git
