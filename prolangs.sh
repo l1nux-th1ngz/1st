b9a8e32847160c4d13a489f4138f75a99b6b0f39
l1nux-th1ngz <137152321+l1nux-th1ngz@users.noreply.github.com>
2024-08-13 01:17:27 -0600

    Update prolangs.sh

--- prolangs.sh
+++ prolangs.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+sudo apt-get -y curl wget cmake cmake-extras gcc ninja-build autoconf automake cmake-data
+sudo apt update
 sudo apt-get -y install python3-full python3-pip libpangocairo-1.0-0 python3-cffi python3-xcffib
 
 # Rust
@@ -10,8 +12,6 @@ EOF
 # Apply changes to the current session
 source /etc/profile.d/rust.sh
 
-
-
 # GO
 wget https://go.dev/dl/go1.22.6.linux-amd64.tar.gz
 sudo tar -C /usr/local -xzf go1.22.6.linux-amd64.tar.gz
@@ -20,7 +20,6 @@ rm go1.22.6.linux-amd64.tar.gz
 echo 'export PATH=$PATH:/usr/local/go/bin' | sudo tee -a /etc/profile
 # Apply changes system-wide
 sudo /bin/bash -c "source /etc/profile"
-
 
 # NodeJS
 curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash - 
@@ -55,6 +54,15 @@ curl -fsSL https://github.com/rbenv/rbenv-installer/raw/HEAD/bin/rbenv-installer
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(rbenv init -)"' >> ~/.bashrc
 source ~/.bashrc
+
+# Download Luarocks source
+wget https://luarocks.org/releases/luarocks-3.11.1.tar.gz
+tar zxpf luarocks-3.11.1.tar.gz
+cd luarocks-3.11.1
+./configure && make && sudo make install
+sudo luarocks install luasocket
+cd ..
+rm -rf luarocks-3.11.1 luarocks-3.11.1.tar.gz
 
 # Download JDK packages
 wget https://download.oracle.com/java/22/latest/jdk-22_linux-x64_bin.deb
